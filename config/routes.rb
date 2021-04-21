0cc30620b9929a876bdce9d27ebfd0475b4abb4d
Dipeshtwis <deepesh.97kumar@gmail.com>
2021-04-22 02:57:35 +0530

    Add user controller, route, and migrate the user table

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,11 @@
 Rails.application.routes.draw do
   root "home#index"
-
+  
+  namespace :api do
+    namespace :v1 do
+      resources :users, only: %i[create]
+    end
+  end
+  
   get '*path', to: 'home#index', via: :all
 end
--- db/schema.rb
+++ db/schema.rb
@@ -15,8 +15,8 @@ ActiveRecord::Schema.define(version: 2021_04_21_205434) do
   create_table "users", force: :cascade do |t|
     t.string "first_name", null: false
     t.string "last_name"
-    t.string "email"
-    t.string "password_digest"
+    t.string "email", null: false
+    t.string "password_digest", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
