f4dd9a20d7fc6abcb6949d0aafc85abefcd333a5
Dipeshtwis <deepesh.97kumar@gmail.com>
2021-04-21 20:48:57 +0530

    Setup tailwindcss

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -3,6 +3,7 @@
 // a relevant structure within app/javascript and only use these pack files to reference
 // that code so it'll be compiled.
 
+import "../stylesheets/application.scss"
 require("@rails/ujs").start()
 require("@rails/activestorage").start()
 require("channels")
