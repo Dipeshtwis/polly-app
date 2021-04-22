f6a3849c9205294f87a6db6ac77883f34864025d
Dipeshtwis <deepesh.97kumar@gmail.com>
2021-04-22 15:43:41 +0530

    Add poll model and migration

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_04_21_205434) do
+ActiveRecord::Schema.define(version: 2021_04_22_094825) do
+
+  create_table "polls", force: :cascade do |t|
+    t.text "title", null: false
+    t.integer "user_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "first_name", null: false
@@ -21,4 +28,5 @@ ActiveRecord::Schema.define(version: 2021_04_21_205434) do
     t.datetime "updated_at", precision: 6, null: false
   end
 
+  add_foreign_key "polls", "users", on_delete: :cascade
 end
